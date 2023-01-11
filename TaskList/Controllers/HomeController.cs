3d8d25df9afd0ab86ba827819db959d5742f57ab
potap <potap@DESKTOP-K5AE70P>
2023-01-12 02:26:28 +0300

    Добавьте файлы проекта.

---
+++ TaskList/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TaskList.Models;
+
+namespace TaskList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult TaskList()
+        {
+            var tasks = new List<Models.Task>()
+            {
+                new Models.Task{Description = "Уборка", Date = DateTime.Now.AddDays(1)},
+                new Models.Task{Description = "Посуда", Date = DateTime.Now.AddDays(1)}
+            };
+
+            return View(tasks);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TaskList/Models/Task.cs
@@ -0,0 +1,23 @@
+﻿namespace TaskList.Models
+{
+    public class Task
+    {
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public Task()
+        {
+            Description= string.Empty;
+            Date = DateTime.Now;
+            IsCompleted = false;
+        }
+
+        public Task(string description, DateTime date)
+        {
+            Description = description;
+            Date = date;
+            IsCompleted = false;
+        }
+    }
+}
